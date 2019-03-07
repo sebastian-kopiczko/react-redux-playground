d4907d5b9e7b1beb22b84c261c534ef77d6518e2
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-03-07 18:13:59 +0100

    toggle post body by clicking in header

--- src/components/Post/PostItem/PostItem.jsx
+++ src/components/Post/PostItem/PostItem.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const PostItem = (props) => {
-  const { id, title, body } = props;
-  return (
-      <div key={id} className="post">
-        <h2 className="post__title">{title}</h2>
-        <p>{body}</p>
-      </div>
-  )
+class PostItem extends Component  {
+  constructor(props){
+    super(props);
+    this.state = {
+      showPostBody: true
+    }
+  }
+  render(){
+    const { id, title, body } = this.props;
+    return (
+        <div key={id} className="post">
+          <h2 className="post__title"
+              onClick={()=>this.setState({showPostBody: !this.state.showPostBody})}
+          >{title}</h2>
+          {this.state.showPostBody ? <p>{body}</p>: null}
+        </div>
+    )
+
+  }
 }
 export default PostItem
